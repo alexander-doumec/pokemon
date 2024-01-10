ee5ec9fa24e98c6d3710bd6761b2abf48d7f19bb
alexander-doumec <alexander.doumec@laplateforme.io>
2024-01-10 20:56:21 +0100

    pokemon

--- interface.py
+++ interface.py
@@ -1,58 +1,28 @@
 import pygame
-import sys
-
 pygame.init()
 
-lageur_fenetre = 600
-hauteur_fenetre = 400
-fenetre = pygame.display.set_mode((lageur_fenetre, hauteur_fenetre))
-pygame.display.set_caption("Pokémon")
+#generer la fenetre de notre jeu 
 
-#Couleurs
+pygame.display.set_caption("Pokemon")
+screen = pygame.display.set_mode((1280,1024))
 
-blanc = (255, 255, 255)
-rouge = (255, 0, 0)
+#importer l'image pour charger l'arriere plan de notre jeu 
+background = pygame.image.load('image/fond.jpg')
 
-#Police
+running = True
 
-police = pygame.font.Font(None, 36)
+#boucle tant que condition est vrai 
+while running:
+    #appliquer l'arriere plan de notre jeu 
+    screen.blit(background, (0,0))
 
-#Fonction pour afficher le texte sur le bouton
-def afficher_texte_bouton(texte, couleur, position):
-    texte_surface = police.render(texte, True, couleur)
-    text_rect = texte_surface.get_rect(center=position)
-    fenetre.blit((texte_surface, text_rect))
-    
-#boucle principale
+    #mettre à jour l'écran 
+    pygame.display.flip()
 
-while True:
-    fenetre.fill(blanc)
-    
-    #gestion des évenements
+    #si le joueur ferme la fenetre 
     for event in pygame.event.get():
-        if event.type == pygame.Quit:
+        #verifier que l'evenement est fermeture de fenetre 
+        if event.type == pygame.QUIT:
+            running = False
             pygame.quit()
-            sys.exit()
-            
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = event.pos
-            
-            #verifications des clic sur le bouton 
-            if 100 < x < 300 and 120 < y < 100:
-                print("Jouer")
-                #code pour lancer le jeu
-                
-            elif 100 < x < 300 and 100 < y < 170:
-                print("Pokedex")
-                #mettre le code pour affciher le pokédex
-            
-            elif 100 < x < 300 and 190 < y < 240:
-                print("Quitter")
-                pygame.quit()
-                sys.exit()
-                
-#afficher les boutons 
-
-    pygame.draw.rect(fenetre, noir, (100, 50, 200, 50))
-    afficher_texte_bouton("Jouer, ")
-                 
+            print("Fermeture du jeu")
